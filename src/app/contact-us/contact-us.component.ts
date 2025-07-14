2b1d317033a1492758686476674929aa17e91a04
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2025-07-14 10:30:27 +0530

    ad

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -7,37 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
-// onSubmit(form: any) {
-//   if (form.valid) {
-//     console.log('Form Submitted:', form.value);
-//     alert('Thanks! We will get back to you soon.');
-//     form.reset();
-//   }
-// }
- formData = {
-    name: '',
-    phone: '',
-    email: '',
-    company: '',
-    subject: '',
-    question: ''
-  };
-
-  submitForm() {
-    const toEmail = 'sudeeppalai28.com'; // Replace with your actual email
-
-    const subject = encodeURIComponent(this.formData.subject);
-    const body = encodeURIComponent(
-      `Name: ${this.formData.name}\n` +
-      `Phone: ${this.formData.phone}\n` +
-      `Email: ${this.formData.email}\n` +
-      `Company: ${this.formData.company}\n\n` +
-      `Question:\n${this.formData.question}`
-    );
-
-    const mailtoLink = `mailto:${toEmail}?subject=${subject}&body=${body}`;
-
-    window.location.href = mailtoLink;
+onSubmit(form: any) {
+  if (form.valid) {
+    console.log('Form Submitted:', form.value);
+    alert('Thanks! We will get back to you soon.');
+    form.reset();
   }
+}
 
 }
