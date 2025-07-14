ab39f3dee44b1d03ae7970470bba5cff14f9075a
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2025-07-14 12:23:44 +0530

    sm

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -10,12 +10,67 @@ import { ActivatedRoute, Route } from '@angular/router';
 export class ExploreComponent implements OnInit{
   
 cityName='' 
+slide:any
+ slides=[
+    {
+      src:'assets/india gate.jpg',
+      title:'Explore Delhi',
+      para:'From historical wonders to bustling markets – explore the heart of India with My Journey Cabs. Book your ride in Delhi now!',
+      para2:'Perfect for city rides and daily travel – enjoy comfort and affordability with our Sedan range.',
+      city:'Delhi'
+
+
+    },
+    {
+      src:'/assets/bangalore.jpg',
+      title:'Explore Bangalore',
+      para:'From tech parks to tranquil gardens – discover every corner of Bangalore with My Journey Cabs. Your smooth city ride awaits.',
+      para2:'Make every journey grand – ride in style with our premium luxury cars for weddings, VIP travel, and special events',
+      city:'Bangalore'
+
+
+    },
+    {
+      src:'assets/gateway of india.jpg',
+      title:'Explore Mumbai',
+      para:'From Marine Drive to Bollywood vibes – experience the spirit of Mumbai with My Journey Cabs. Comfort and class on every drive!',
+      para2:'Spacious, powerful, and road-trip ready – our SUVs are built for adventure and long-distance comfort.',
+      city:'Mumbai'
+
+
+    },
+    {
+    src:'assets/chennai.jpg',
+    city:'Chennai'
+  },
+   {
+    src:'assets/goa.jpg',
+    city:'Goa'
+  },
+   {
+    src:'assets/bhubaneshwar.jpg',
+    city:'Bhubaneshwar'
+  },
+   {
+    src:'assets/srinagar.jpg',
+    city:'Srinagar'
+  },
+   {
+    src:'assets/indore.jpg',
+    city:'Indore'
+  },
+   {
+    src:'assets/jaipur.jpg',
+    city:'Jaipur'
+  },
+  ]
 
 constructor(private route:ActivatedRoute){}
 ngOnInit(): void {
   this.cityName=this.route.snapshot.paramMap.get('city') || ''
-  
-  
+  const matchedSlide=this.slides.find(s=>s.city===this.cityName);
+  console.log(matchedSlide)
+  this.slide=matchedSlide
 
 
 }
